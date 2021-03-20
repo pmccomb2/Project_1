98ec477b008aea3c6f5e87185658fe2e453d8f3f
Jhader2 <71098789+Jhader2@users.noreply.github.com>
2021-03-20 15:34:11 -0400

    Enemy first commit
    -Created Enemy GameObject
    -Added Sprite Renderer
    -Added Rigidbody 2d
    - Created Enemy script
    -Locked rotatioin
    -Enemy follows player

---
+++ Assets/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float moveSpeed = 0.01f;
+
+    private Rigidbody2D rbe;
+
+    public Transform player;
+
+    private Vector2 movement;
+
+    public float lockPos;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbe  = this.GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Vector3 direction = player.position - transform.position;
+       float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+       rbe.rotation = angle;
+       direction.Normalize();
+       movement = direction;
+
+       transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,lockPos,lockPos);
+    }
+    void moveCharacter(Vector2 direction)
+    {
+        rbe.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+    void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+}
